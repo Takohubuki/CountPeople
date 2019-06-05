19658b9deab679956a27a60ab7c80d2955c52ef4
Takohubuki <932421804@qq.com>
2019-06-05 12:38:08 +0800

    2019-06-05 12:35

---
+++ src/main/java/com/cnsoft/counting/bean/WarningInfo.java
@@ -0,0 +1,43 @@
+package com.cnsoft.counting.bean;
+
+public class WarningInfo {
+    private Integer id;
+
+    private String video_id;
+
+    private String warning_start;
+
+    private String warning_end;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getVideo_id() {
+        return video_id;
+    }
+
+    public void setVideo_id(String video_id) {
+        this.video_id = video_id == null ? null : video_id.trim();
+    }
+
+    public String getWarning_start() {
+        return warning_start;
+    }
+
+    public void setWarning_start(String warning_start) {
+        this.warning_start = warning_start == null ? null : warning_start.trim();
+    }
+
+    public String getWarning_end() {
+        return warning_end;
+    }
+
+    public void setWarning_end(String warning_end) {
+        this.warning_end = warning_end == null ? null : warning_end.trim();
+    }
+}
---
+++ src/main/java/com/cnsoft/counting/mappers/WarningInfoMapper.java
@@ -0,0 +1,30 @@
+package com.cnsoft.counting.mappers;
+
+import com.cnsoft.counting.bean.WarningInfo;
+import com.cnsoft.counting.bean.WarningInfoExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface WarningInfoMapper {
+    long countByExample(WarningInfoExample example);
+
+    int deleteByExample(WarningInfoExample example);
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(WarningInfo record);
+
+    int insertSelective(WarningInfo record);
+
+    List<WarningInfo> selectByExample(WarningInfoExample example);
+
+    WarningInfo selectByPrimaryKey(Integer id);
+
+    int updateByExampleSelective(@Param("record") WarningInfo record, @Param("example") WarningInfoExample example);
+
+    int updateByExample(@Param("record") WarningInfo record, @Param("example") WarningInfoExample example);
+
+    int updateByPrimaryKeySelective(WarningInfo record);
+
+    int updateByPrimaryKey(WarningInfo record);
+}
